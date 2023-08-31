480644c03fdd7083722d51bb968e476881d7fb13
PedroMF <pedrolucas3049@gmail.com>
2023-08-31 11:41:44 -0300

    Fim da Aula

--- MarkDown/javascrpit.md
+++ MarkDown/javascrpit.md
@@ -8,9 +8,9 @@
 | while | repetição |
 | do | executa algo em uma repetção |
 | console/write | faz algo aparecer no terminal |
-| | |
-| | |
-| | | 
+| alert | cria um alerta sempre que recarrega a pagina |
+| prompt | recebe dados do usuarios |
+| confrim | cria uma tomada de decisão pro usuario | 
 | | | 
 | | | 
 | | | 
---
+++ pastaHTML/atividade11/script1.js
@@ -0,0 +1,16 @@
+console.log("usnado o console")
+let numero1 = "pedro";
+let numero2 = 2;
+const numero3 = 10;
+var soma = numero1 - numero2
+
+var nome = "pedro";
+var caneca = {
+    nome: "goio",
+    cor: "preta",
+    educativa: true
+}
+
+var exvetor = ['caneca', 'cafe'] 
+console.log(exvetor)
+console.log(typeof(exvetor))
